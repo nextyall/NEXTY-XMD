25942fa4a4e6e8712c3a4a8acd30ec4205d1ef2c
nextyall <helloworl_d@outlook.com>
2025-08-22 13:47:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-const config = {
-  sessionId: process.env.SESSION_ID || "",
-  owner: "923192084504", // ✅ tumhara number
-  prefix: ".",
-  botName: "NEXTY-XMD"
+module.exports = {
+  sessionId: process.env.SESSION_ID || "NEXTY-XMD-SESSION", // Apna Session ID Heroku pe Config Vars me daalna
+  owner: "923192084504", // Tumhara number (country code ke sath)
+  botName: "NEXTY-XMD",  // Bot ka naam
+  prefix: ".",           // Commands ka prefix
 };
-
-module.exports = config;
